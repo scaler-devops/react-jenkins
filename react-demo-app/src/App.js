0df54e861160228fd23d19b8df5fce88fa70f1c0
rashmika <rashmika@scaler.com>
2025-11-27 14:05:32 +0530

    Create App.js

---
+++ react-demo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <img src={logo} alt="React Logo" width="120" />
+      <h1>🚀 CI/CD Pipeline with Jenkins</h1>
+      <p>This is a demo React app built using Jenkins Freestyle Job.</p>
+    </div>
+  );
+}
+
+export default App;
